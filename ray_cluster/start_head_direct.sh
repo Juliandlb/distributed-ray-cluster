110e6f7a12d2dd11acbd967e723f86efd74623c1
Juliandlb <juliandlb@gmail.com>
2025-07-31 14:28:05 +0200

    feat: Use a local tmp directory for Ray

--- ray_cluster/start_head_direct.sh
+++ ray_cluster/start_head_direct.sh
@@ -9,8 +9,11 @@ PUBLIC_IP=$(curl -s ifconfig.me)
 
 echo "📍 Public IP: $PUBLIC_IP"
 
+# Set PYTHONPATH
+export PYTHONPATH=/mnt/data/distributed-ray-cluster/ray_cluster/ray_demo_env/lib/python3.11/site-packages
+
 # Start Ray head node
-ray start --head --port=6379 --ray-client-server-port=10001 --dashboard-host=0.0.0.0 --dashboard-port=8265
+/mnt/data/distributed-ray-cluster/ray_cluster/ray_demo_env/bin/python3.11 -m ray.scripts.scripts start --head --port=6379 --ray-client-server-port=10001 --dashboard-host=0.0.0.0 --dashboard-port=8265 --temp-dir=./tmp
 
 echo ""
 echo "🎯 CLUSTER READY FOR REMOTE WORKERS!"
